555d2b308c41d880fed014f2cbe0927d098fa524
unknown <>
2023-01-19 10:38:31 +0300

    some

--- sem_2/1/task/01.py
+++ sem_2/1/task/01.py
@@ -1,24 +1,102 @@
-"""
-Автомобиль
-имеет цвет
-имеет мощность дивгателя
-имеет максимальный запас бензина в баке
-имеет расход (например 9.3 литра на 100 км)
+class Car:
+    """
+    класс автомобиль
+    """
+    color = 'white'  # цвет
+    engine_power = 90  # мощность двигателя
+    tank_capacity = 50  # объем бака
+    fuel_consumption = 9.3  # расход топлива на 100 км
+    fuel_now = 0  # топлива в баке сейчас
 
-можно перекрасить
-можно заправить N литров
-можно проехать N киллометров
+    def __init__(self):
+        pass
 
-можно узнать цвет
-мощность
-сколько осталось литров в баке
-узнать расход
-"""
+    def set_color(self, color):
+        """
+        перекрасить автомобиль
+        """
+        pass
 
-"""
-Ваши действия подробно
-1. Создаем класс Автомобиль
-2. Обязательно метод __init__
-3. В __init__ передаются все признаки этого автобиля
-4. Пишем функцию, которая позволяет "можно узнать цвет"
-"""
+    def get_color(self):
+        """
+        узнать цвет
+        """
+        pass
+
+    def get_fuel_now(self):
+        """
+        :return: топлива есть сейчас
+        """
+        pass
+
+    def refuel(self, liters_of_fuel):
+        """
+        заправить liters_of_fuel топлива
+        :param liters_of_fuel:
+        :return:
+        """
+        pass
+
+    def get_engine_power(self):
+        """
+        узнать мощность двигателя
+        :return:
+        """
+        pass
+
+    def get_fuel_consumption(self):
+        """
+        узнать расход топлива
+        :return:
+        """
+        pass
+
+
+def test_init_1():
+    Car(color='white',
+        engine_power=90,
+        tank_capacity=50,
+        fuel_consumption=9.3,
+        fuel_now=0)
+
+
+# test_init_1()
+
+def test_init_2():
+    Car(color='white')
+
+
+# test_init_2()
+def test_color_1():
+    c = Car(color='white',
+            engine_power=90,
+            tank_capacity=50,
+            fuel_consumption=9.3,
+            fuel_now=0)
+    assert c.get_color() == 'white'
+
+
+# test_color_1()
+def test_color_2():
+    c = Car(color='white',
+            engine_power=90,
+            tank_capacity=50,
+            fuel_consumption=9.3,
+            fuel_now=0)
+    assert c.get_color() == 'white'
+    c.set_color('red')
+
+
+# test_color_2()
+def test_color_3():
+    c = Car(color='white',
+            engine_power=90,
+            tank_capacity=50,
+            fuel_consumption=9.3,
+            fuel_now=0)
+    assert c.get_color() == 'white'
+    c.set_color('red')
+
+    assert c.get_color() == 'red'
+
+# test_color_3()
